b5824e46cda3c60171db699be8a96f7931cb0c8f
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-11-04 20:23:14 +0000

    arreglando primera expectativa

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -5,12 +5,12 @@ require 'rspec'
 describe Matrix do
 
   before :each do
-  	@matrix1 = Matrix.new("9 3 2,4 6 7")
-    @matrix2 = Matrix.new("3 5 7,8 9 3")
+  	@matrix1 = Matrix.new("9 3 2\n4 6 7")
+    @matrix2 = Matrix.new("3 5 7\n8 9 3")
   end
 
     it "La matriz se crea correctamente" do
-      @matrix1.imp.should eq("[9, 3, 2], [4, 6, 7]]")
+      @matrix1.show.should eq("[[9, 3, 2], [4, 6, 7]]")
     end
 end
 
